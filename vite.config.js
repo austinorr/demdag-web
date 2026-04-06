09d0d0657459216039359db66fffba1d19985a72
Austin Orr <aorr@geosyntec.com>
2026-04-06 12:45:30 -0700

    add minify to gl files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
+  plugins: [glsl({ minify: true })],
   base: "/demdag-web/",
   assetsInclude: ["**/*{.tif,.jpg}"],
   build: {
